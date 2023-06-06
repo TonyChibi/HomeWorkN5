725a326e963bf00ad7b5b97646ce8f38ee33bcae
GlazHorsez <to5gogol@gmail.com>
2023-06-06 11:54:16 +0300

    added task N 34

---
+++ taskN34/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+
+int [] MakeArray(){
+        Console.Write("insert the length: ");
+        int len = int.Parse(Console.ReadLine()!);
+    int []arr=new int [len];
+    for (int i=0;i<len;i++){
+    arr[i]=new Random().Next(100,1000);    
+    }
+    return arr;
+}
+
+void AllEvens(int []array){
+    int count=0;
+    foreach(int elem in array){
+        if(elem%2==0)count++;
+    }
+Console.WriteLine($"in the array {count} even numbers");
+
+}
+int []arr= MakeArray();
+Console.WriteLine(string.Join(",",arr));
+AllEvens(arr);
